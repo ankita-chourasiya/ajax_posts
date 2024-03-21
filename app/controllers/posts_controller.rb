deb33c472b94da9fc019c7fc968b215ecb3bcd99
Ankita <ankitachourasiya11@gmail.com>
2024-03-21 17:03:08 +0530

    Create AJAX request when create a post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,23 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      respond_to do |format|
+        format.js
+        format.html { redirect_to new_post_path }
+      end
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
